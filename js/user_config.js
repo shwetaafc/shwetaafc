b839e21ef967cab86a565b0a85c99c1080a26b87
Rishabh Agrawal <tanujvrshn@gmsil.com>
2016-10-02 01:31:32 +0530

    v1.0

---
+++ js/user_config.js
@@ -0,0 +1,16 @@
+/*===========================================================
+  Javascript Configuration File
+===========================================================*/
+
+/**
+ * Latitude, Longitude and zoom factor for the Google Map
+ * Go to http://itouchmap.com to find your coordinates
+ *
+ * Zoom: 5 (far away) <-> 15 (very close)
+*/
+
+mapLatitude = 40.713772;
+mapLongitude = -74.005275;
+mapZoom = 13;
+
+
